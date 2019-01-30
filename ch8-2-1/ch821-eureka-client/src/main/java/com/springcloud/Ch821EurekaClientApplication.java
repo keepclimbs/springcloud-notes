6a64986b03d672bffb386000480ed3c7a72b8b6a
keepclimbs <499429059@qq.com>
2019-01-30 11:27:33 +0800

    oauth2.0+ jwt + zuul

---
+++ ch8-2-1/ch821-eureka-client/src/main/java/com/springcloud/Ch821EurekaClientApplication.java
@@ -0,0 +1,60 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * @author: song biao wei
+ * @date: 2019/1/29 17:50
+ * @description:
+ */
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableResourceServer
+public class Ch821EurekaClientApplication extends ResourceServerConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch821EurekaClientApplication.class, args);
+    }
+
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/test")
+                .hasAuthority("WRIGTH_READ");
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("WRIGTH").tokenStore(jwtTokenStore());
+    }
+
+    @Bean
+    protected JwtAccessTokenConverter jwtTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("springcloud123");
+        return converter;
+    }
+
+    @Bean
+    public TokenStore jwtTokenStore() {
+        return new JwtTokenStore(jwtTokenConverter());
+    }
+}
---
+++ ch8-2-1/ch821-eureka-client/src/main/java/com/springcloud/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.springcloud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * @author: song biao wei
+ * @date: 2019/1/29 13:22
+ * @description:
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(HttpServletRequest request) {
+        System.out.println("----------------header----------------");
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            System.out.println(key + ": " + request.getHeader(key));
+        }
+        System.out.println("----------------header----------------");
+        return "hellooooooooooooooo!";
+    }
+}
